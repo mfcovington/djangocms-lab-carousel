6a6e2ff793e4961dea08dc0e7f73949e1b4d130b
Mike Covington <mfcovington@gmail.com>
2015-04-16 18:07:04 -0700

    Register SlideAdmin

--- cms_lab_carousel/admin.py
+++ cms_lab_carousel/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from cms_lab_carousel.models import Carousel
+from cms_lab_carousel.models import Carousel, Slide
 
 class CarouselAdmin(admin.ModelAdmin):
     fieldset_frame = ('Carousel Frame', {
@@ -46,5 +46,57 @@ class CarouselAdmin(admin.ModelAdmin):
     search_fields = ['title']
 
 admin.site.register(Carousel, CarouselAdmin)
+
+
+class SlideAdmin(admin.ModelAdmin):
+
+    fieldset_basic = ('Basic Slide Info', {
+        'fields': [
+            'carousel',
+            'title',
+            'subtitle',
+            'description',
+            'image',
+            'image_is_downloadable',
+        ],
+    })
+
+    fieldset_article = ('Scientific Article Info', {
+        'fields': [
+            'pdf',
+            'pubmed_url',
+            'article_url',
+            'journal_name',
+        ],
+    })
+
+    fieldset_other_url = ('Other URL', {
+        'fields': [
+            'other_url',
+            'other_url_label',
+            'other_url_color',
+        ],
+        'classes': ['collapse'],
+    })
+
+    fieldset_publish = ('Publish Settings', {
+        'fields': [
+            'publish_slide',
+            'publish_datetime',
+        ],
+    })
+
+    fieldsets = [
+        fieldset_basic,
+        fieldset_article,
+        fieldset_other_url,
+        fieldset_publish,
+    ]
+
+    list_display = ['title', 'carousel', 'publish_slide', 'publish_datetime' ]
+    list_filter = ['publish_slide', 'journal_name']
+    search_fields = ['title', 'subtitle', 'description']
+
+admin.site.register(Slide, SlideAdmin)
 
 admin.site.site_header = 'CMS Lab Carousel Administration'
