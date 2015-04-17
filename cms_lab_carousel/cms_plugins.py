dd51b2410e7786f3e79688aacd9779d3833d59ad
Mike Covington <mfcovington@gmail.com>
2015-04-17 13:20:18 -0700

    Add Carousel admin menus to CMS Toolbar

--- cms_lab_carousel/cms_plugins.py
+++ cms_lab_carousel/cms_plugins.py
@@ -2,6 +2,7 @@ from cms.plugin_base import CMSPluginBase
 from cms.plugin_pool import plugin_pool
 from cms_lab_carousel.models import CarouselPlugin
 
+from django.core.urlresolvers import reverse
 from django.utils import timezone
 
 from cms_lab_carousel.models import Carousel, Slide
@@ -13,11 +14,6 @@ class CMSCarouselPlugin(CMSPluginBase):
     render_template = "cms_lab_carousel/plugin.html"
 
     def render(self, context, instance, placeholder):
-        # context.update({'instance': instance,})
-
-        # Slide.objects.filter(carousel__id=instance.id)
-        # context['current_list'] = Scientist.objects.filter(current=True)
-
         published_slides_list = Slide.objects.filter(
             carousel__id=instance.carousel.id,
             publish_datetime__lte=timezone.now(),
@@ -28,8 +24,20 @@ class CMSCarouselPlugin(CMSPluginBase):
             'instance': instance,
             'published_slides_list': published_slides_list,
         })
+
+        menu = context['request'].toolbar.get_or_create_menu('carousel-menu', 'Carousel')
+
+        url_change = reverse('admin:cms_lab_carousel_slide_changelist')
+        url_addnew = reverse('admin:cms_lab_carousel_slide_add')
+        menu.add_sideframe_item('Edit Slides', url=url_change)
+        menu.add_modal_item('Add New Slide', url=url_addnew)
+        menu.add_break()
+
+        url_change = reverse('admin:cms_lab_carousel_carousel_changelist')
+        url_addnew = reverse('admin:cms_lab_carousel_carousel_add')
+        menu.add_sideframe_item('Edit Carousels', url=url_change)
+        menu.add_modal_item('Add New Carousel', url=url_addnew)
+
         return context
 
 plugin_pool.register_plugin(CMSCarouselPlugin)
-
-
